3278fb62d027f16ac7f2eb901d2b428f95fba4ce
Ion Glavan <ionelglavan@gmail.com>
2018-08-28 16:55:01 +0300

    added link to each adress in the navigation and added helper url in autoload.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -22,17 +22,12 @@
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="<?php echo base_url(); ?>about">about</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
+
                 </ul>
             </div>
         </nav>
